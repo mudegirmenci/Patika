fc015e666298cda9dbc4c80e3c92ea5ed423982e
mudegirmenci <mudegirmenci@gmail.com>
2022-04-14 19:02:55 +0300

    Node.js ödev 1 eklendi

---
+++ Ödevler/ödev1/README.md
@@ -0,0 +1,4 @@
+### Ödev 1
+### Node.JS Komut Satırı Kullanımı
+
+Dairenin alanını yarıçap değerini konsoldan parametre olarak girerek bulma.
---
+++ Ödevler/ödev1/alan.js
@@ -0,0 +1,8 @@
+
+const yaricap = process.argv[2]
+function getAlan(yaricap){
+    let alan = Math.PI*yaricap
+    console.log(alan)
+}
+
+getAlan(yaricap);
