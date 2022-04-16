c407e5cd8199ec13cbc3ae373c3b515c48596e70
mudegirmenci <mudegirmenci@gmail.com>
2022-04-16 15:21:30 +0300

    index.js iyileştirme

--- courses/nodeJs/odev5/index.js
+++ courses/nodeJs/odev5/index.js
@@ -7,16 +7,16 @@ const server = http.createServer( (req,resp) => {
     console.log(url)
 
     if( url === '/')  { 
-        resp.writeHead(200,{'Content-Type': 'text/html'})
+        resp.writeHead(200,{'Content-Type': 'text/html;  charset=utf-8'})
         resp.write("<h2>INDEX SAYFASINA HOŞGELDİNİZ</h2>") 
     } else if ( url === '/about') {
-        resp.writeHead(200,{'Content-Type': 'text/html'})
+        resp.writeHead(200,{'Content-Type': 'text/html;  charset=utf-8'})
         resp.write("<h2>ABOUT SAYFASINA HOŞGELDİNİZ</h2>")
     } else if ( url === '/contact') {
-        resp.writeHead(200,{'Content-Type': 'text/html'})
+        resp.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'})
         resp.write("<h2>CONTACT SAYFASINA HOŞGELDİNİZ</h2>")
     } else {
-        resp.writeHead(404,{'Content-Type': 'text/html'})
+        resp.writeHead(404,{'Content-Type': 'text/html; charset=utf-8'})
         resp.write("404 SAYFA BULUNAMADI")
     }
         console.log('Bir istek gönderildi.')
