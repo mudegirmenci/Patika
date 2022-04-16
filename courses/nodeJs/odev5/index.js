8016d11b4a1f7d52fbc892c9ea167807c56f4030
mudegirmenci <mudegirmenci@gmail.com>
2022-04-16 15:17:51 +0300

    Ödev 5 yapıldı

---
+++ courses/nodeJs/odev5/index.js
@@ -0,0 +1,31 @@
+import http from "http";
+
+const server = http.createServer( (req,resp) => {
+    
+    const url = req.url;
+
+    console.log(url)
+
+    if( url === '/')  { 
+        resp.writeHead(200,{'Content-Type': 'text/html'})
+        resp.write("<h2>INDEX SAYFASINA HOŞGELDİNİZ</h2>") 
+    } else if ( url === '/about') {
+        resp.writeHead(200,{'Content-Type': 'text/html'})
+        resp.write("<h2>ABOUT SAYFASINA HOŞGELDİNİZ</h2>")
+    } else if ( url === '/contact') {
+        resp.writeHead(200,{'Content-Type': 'text/html'})
+        resp.write("<h2>CONTACT SAYFASINA HOŞGELDİNİZ</h2>")
+    } else {
+        resp.writeHead(404,{'Content-Type': 'text/html'})
+        resp.write("404 SAYFA BULUNAMADI")
+    }
+        console.log('Bir istek gönderildi.')
+   // resp.write("Merhaba")
+    resp.end()
+})
+
+
+const port = 5000
+server.listen(port, () => {
+    console.log(`Sunucu port  ${port} de başlatıldı`)
+})
