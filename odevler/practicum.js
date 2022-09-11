b8e8837316bb7dc2e6bb31459ba60a13b6fcd131
Mustafa Değirmenci <77432211+mudegirmenci@users.noreply.github.com>
2022-09-11 14:05:14 +0300

    Create practicum.js

---
+++ odevler/practicum.js
@@ -0,0 +1,12 @@
+function oddishOrEvenish(sayi) {
+  
+        if(sayi%2 === 0) {
+            console.log("Even.")
+        } else if(sayi%2 === 1) {
+            console.log("Odd")
+        } else {
+            console.log("Lütfen geçerli bir sayı giriniz!")
+        }
+        return 0
+}
+oddishOrEvenish(43)
